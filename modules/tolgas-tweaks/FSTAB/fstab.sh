5cc88d67eb8d3a1f97435f88cc53fb75ea58f859
Tolga Erok <kingtolga@gmail.com>
2025-03-24 12:07:14 +0800

    (ツ)_/¯ Edit: 24-03-2025 12:07:14 PM

--- modules/tolgas-tweaks/FSTAB/fstab.sh
+++ modules/tolgas-tweaks/FSTAB/fstab.sh
@@ -47,13 +47,14 @@ done
 # Samba mount entries for /etc/fstab
 fstab_entries=(
     ""
-    "# --------------------------  NETWORK SHARES ------------------------------------ #"
+    "# --------------------------  NETWORK SHARES  ------------------------------------ #"
     "//192.168.0.1/tolga                        /mnt/Optus cifs credentials=/etc/samba/Windows_credentials.conf,vers=1.0,uid=1000,gid=1000,file_mode=0777,dir_mode=0777,iocharset=utf8,cache=loose,noserverino,actimeo=60 0 0"
     "//192.168.0.17/Public/RELATIONSHIPS        /mnt/Relationships cifs credentials=/etc/samba/QNAP_credentials.conf,vers=3.0,uid=1000,gid=1000,file_mode=0777,dir_mode=0777,iocharset=utf8,cache=loose,noserverino,actimeo=60 0 0"
     "//192.168.0.18/Documents                   /mnt/Mint cifs credentials=/etc/samba/Windows_credentials.conf,vers=3.0,uid=1000,gid=1000,file_mode=0777,dir_mode=0777,iocharset=utf8,cache=loose,noserverino,actimeo=60 0 0"
     "//jack-sparrow.local/Public                /mnt/Public cifs credentials=/etc/samba/QNAP_credentials.conf,vers=3.0,uid=1000,gid=1000,file_mode=0777,dir_mode=0777,iocharset=utf8,cache=loose,noserverino,actimeo=60 0 0"
     "//jack-sparrow.local/Public/PC-PORTAL      /mnt/QNAP cifs credentials=/etc/samba/QNAP_credentials.conf,vers=3.0,uid=1000,gid=1000,file_mode=0777,dir_mode=0777,iocharset=utf8,cache=loose,noserverino,actimeo=60 0 0"
-    "#"
+    ""
+    "# --------------------------      TOOLS     ------------------------------------ #"
     "#     To Reload FSTB"
     "#      sudo systemctl daemon-reload && sudo mount -a"
     "#"
