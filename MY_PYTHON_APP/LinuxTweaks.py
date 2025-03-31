032bea7b8720aa7e3bbdcf35c0e1a5170a258f56
Tolga Erok <kingtolga@gmail.com>
2025-03-31 11:02:50 +0800

    (ツ)_/¯ Edit: 31-03-2025 11:02:50 AM

--- MY_PYTHON_APP/LinuxTweaks.py
+++ MY_PYTHON_APP/LinuxTweaks.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+# Tolga Erok
+# 26-3-2025
+# Version:                  3.0
+
+# APP IMAGE LOCATION:      /usr/local/bin/LinuxTweaks/images/LinuxTweak.png
+# APP LOCATION:            /usr/local/bin/LinuxTweaks/LinuxTweaks.py
+# PYTHON ONLINE FORMATTER: https://codebeautify.org/python-formatter-beautifier#
+# SYMLINK:                 sudo ln -s /usr/local/bin/LinuxTweaks/LinuxTweaks.py /usr/local/bin/linuxtweaks
+# Installer:               curl -sL https://raw.githubusercontent.com/tolgaerok/linuxtweaks/main/MY_PYTHON_APP/installer.sh | sudo bash
 
 import sys
 import subprocess
