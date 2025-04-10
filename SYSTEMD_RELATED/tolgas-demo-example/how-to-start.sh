28949efcde3a67b822451556c69589851be02a5f
Tolga Erok <kingtolga@gmail.com>
2025-04-10 23:28:45 +0800

    (ツ)_/¯ Edit: 10-04-2025 11:28:45 PM

--- SYSTEMD_RELATED/tolgas-demo-example/how-to-start.sh
+++ SYSTEMD_RELATED/tolgas-demo-example/how-to-start.sh
@@ -2,3 +2,6 @@ systemctl --user daemon-reexec
 systemctl --user daemon-reload
 systemctl --user enable --now tolga.timer
 systemctl --user start tolga.service
+systemctl --user status tolga.timer
+sudo loginctl enable-linger tolga
+systemctl --user list-timers | grep tolga
