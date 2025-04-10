8d0a97a1cd66618539c5d3459012eb8b980d24d3
Tolga Erok <kingtolga@gmail.com>
2025-04-10 23:39:48 +0800

    (ツ)_/¯ Edit: 10-04-2025 11:39:48 PM

--- SYSTEMD_RELATED/tolgas-demo-example/how-to-start.sh
+++ SYSTEMD_RELATED/tolgas-demo-example/how-to-start.sh
@@ -1,3 +1,8 @@
+# download LinuxTweaks icon
+sudo mkdir -p /usr/local/bin/LinuxTweaks/images
+sudo wget -O /usr/local/bin/LinuxTweaks/images/LinuxTweak.png https://raw.githubusercontent.com/tolgaerok/linuxtweaks/main/MY_PYTHON_APP/images/LinuxTweak.png
+sudo chmod 644 /usr/local/bin/LinuxTweaks/images/LinuxTweak.png
+
 systemctl --user daemon-reexec
 systemctl --user daemon-reload
 systemctl --user enable --now tolga.timer
